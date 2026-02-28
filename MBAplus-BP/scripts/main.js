f137ad379c7ec364da8a4286f7d3b864365a96d3
harasou <ibuki3989@gmail.com>
2026-02-28 15:42:16 +0900

    メモの削除忘れ

--- MBAplus-BP/scripts/main.js
+++ MBAplus-BP/scripts/main.js
@@ -5,11 +5,3 @@ import "./ui.js";
 import "./clicks.js";
 import "./tools.js";
 import "./bow.js";
-
-/*
-==========[開発用メモ]==========
-v4.1.0-aplha
-・serverモジュールをv2.4.0からv2.5.0に変更
-・起動する最低バージョンを26.0に変更
-・まな板の弓を追加
- */
