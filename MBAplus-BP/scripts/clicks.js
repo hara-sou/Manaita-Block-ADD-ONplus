b2db5b7ca52147184e24d90013033f7600e691dc
I <ibuki3989@gmail.com>
2026-08-12 17:26:33 +0900

    Change: 鉱石ブロックをクリックまな板で増やすと原石がドロップするように変更

--- MBAplus-BP/scripts/clicks.js
+++ MBAplus-BP/scripts/clicks.js
@@ -1,6 +1,40 @@
 import { system, ItemStack } from "@minecraft/server";
 import { incClk, noIncList } from "./config";
 
+// ★ 鉱石ブロック → 原石アイテム の対応表
+const oreDropMap = {
+    // 石炭
+    "minecraft:coal_ore":"minecraft:coal",
+    "minecraft:deepslate_coal_ore":"minecraft:coal",
+    // 銅
+    "minecraft:copper_ore":"minecraft:raw_copper",
+    "minecraft:deepslate_copper":"minecraft_raw_copper",
+    // ラピスラズリ
+    "minecraft:lapis_ore":"minecraft:lapis_lazuli",
+    "minecraft:deepslate_lapis_ore":"minecraft:lapis_lazuli",
+    // 鉄
+    "minecraft:iron_ore": "minecraft:raw_iron",
+    "minecraft:deepslate_iron_ore": "minecraft:raw_iron",
+    // 金
+    "minecraft:gold_ore":"minecraft:raw_gold",
+    "minecraft:deepslate_gold_ore":"minecraft:raw_gold",
+    // レッドストーン
+    "minecraft:lit_redstone_ore":"minecraft:redstone",
+    "minecraft:lit_deepslate_redstone_ore":"minecraft:redstone",
+    // ダイヤモンド
+    "minecraft:diamond_ore":"minecraft:diamond",
+    "minecraft:deepslate_diamond_ore":"minecraft:diamond",
+    // エメラルド
+    "minecraft:emerald_ore":"minecraft:emerald",
+    "minecraft:deepslate_emerald_ore":"minecraft:emerald",
+    // ネザークォーツ
+    "minecraft:quartz_ore":"minecraft:quartz",
+    // ネザー金鉱石
+    "minecraft:nether_gold_ore":"minecraft:gold_nugget",
+    // 古代の残骸
+    "minecraft:ancient_debris":"minecraft:netherite_scrap"
+};
+
 system.beforeEvents.startup.subscribe((initEvent) => {
     initEvent.itemComponentRegistry.registerCustomComponent("hraddons:manaita_clicks", {
         onUseOn(event) {
@@ -30,8 +64,9 @@ system.beforeEvents.startup.subscribe((initEvent) => {
             while (remaining > 0) {
                 const spawnAmount = Math.min(remaining, maxStackSize);
 
-                // ★ まずチェスト本体をドロップ
-                const chestItem = new ItemStack(block.typeId, spawnAmount);
+                // ★ 鉱石ブロックの場合は原石を、それ以外はブロック自体をドロップ
+                const dropTypeId = oreDropMap[block.typeId] ?? block.typeId;
+                const chestItem = new ItemStack(dropTypeId, spawnAmount);
                 block.dimension.spawnItem(chestItem, spawnLocation);
 
                 // ★ 中身を spawnAmount 回分ドロップ
